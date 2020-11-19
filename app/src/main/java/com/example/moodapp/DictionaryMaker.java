8b0f795407c8f182331802f4ee617fa961b1c043
MatOgr <matjonne@gmail.com>
2020-11-19 18:10:03 +0100

    JSON seems to work, have to convert whole into String

--- app/src/main/java/com/example/moodapp/DictionaryMaker.java
+++ app/src/main/java/com/example/moodapp/DictionaryMaker.java
@@ -1,5 +1,7 @@
 package com.example.moodapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,12 +12,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class DataLoader {
-    private Map<String, String> moodData;
+public class DictionaryMaker extends AppCompatActivity implements DMInterface {
 
-    // TODO: 18.11.2020 look at the API version reqs
-    public String loadString() throws MalformedURLException {
-        URL urlAddr = new URL("https://moodup.team/test/info.php");
+    // loads site content and formats it to appropriate string
+    public String loadString(String url) {
+        URL urlAddr = null;
+        try {
+            urlAddr = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         ArrayList<String> tablice = new ArrayList<>();
         String unformattedString = null;
@@ -33,7 +39,7 @@ public class DataLoader {
         unformattedString = unformattedString.substring(1, unformattedString.length()-1)
                 .trim()
                 .replaceAll(" \\[    ", "\\[").replaceAll("    ", "");
-        System.out.println(unformattedString + '\n');
+        //System.out.println(unformattedString + '\n');
 
         // getting the arrays out
         int i = 0;
@@ -44,12 +50,12 @@ public class DataLoader {
                     .substring(2, tablice.get(i).length()-1)
                     .replaceAll("\",\"", "=")
                     .replaceAll("\"", ""));
-            System.out.println((i) + " tablica: " + tablice.get(i));
+            //System.out.println((i) + " tablica: " + tablice.get(i));
             i+=1;
         }
         unformattedString = unformattedString.replaceAll(":", "::");
         String result = "";
-        System.out.println("Liczba tablic: " + tablice.size() + "\n\nA oto nasza tablica do slownika:\n");
+        //System.out.println("Liczba tablic: " + tablice.size() + "\n\nA oto nasza tablica do slownika:\n");
         for (String x : unformattedString.split("\",\"")) {
             result += '#' + x.replaceAll("\"", "");
         }
@@ -57,27 +63,25 @@ public class DataLoader {
         for (i = 0; i < tablice.size(); i++) {
             result = result.replaceFirst(String.format("##tablica_%d##", i), tablice.get(i));
         }
-        System.out.println(result.substring(1));
+        //System.out.println(result.substring(1));
 
         return result.substring(1);
     }
 
-
-    public Map<String, String> getMoodData() {
-        return moodData;
-    }
-
-    public void setMoodData(Map<String, String> moodData) {
-        this.moodData = moodData;
-    }
-
+    // creates Map from passed-in String
     public Map<String, String> makeMap(String data) {
         Map<String, String> dataMap = new HashMap<>();
         for (String x : data.split("#")) {
-            System.out.println("Linia: " + x);
+            //System.out.println("Linia: " + x);
             String[] a = x.split("::");
             dataMap.put(a[0], a[1]);
         }
         return dataMap;
+    }
+
+    public String testFunc(String url) {
+
+
+        return " ";
     }
 }
--- app/src/main/java/com/example/moodapp/MainActivity.java
+++ app/src/main/java/com/example/moodapp/MainActivity.java
@@ -1,36 +1,84 @@
 package com.example.moodapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_URL = "https://www.thetreecenter.com/wp-content/uploads/washington-navel-orange-2.jpg";
+    public static final String EXTRA_URL = "https://moodup.team/test/info.php";
+//    public Map<String, String> dictionary = new HashMap<>();
+    private TextView mTextViewResult;      //JSON
+    private RequestQueue mQueue;        //JSON
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //String test = loadString(EXTRA_URL);
+//////////////////////////////
+        mTextViewResult = findViewById(R.id.jsonTest);
+        mQueue = Volley.newRequestQueue(this);
+        jsonParse();
 
+
+
+        /////////////////////////////
         CircularImageView circleImage = findViewById(R.id.circularImg);
         Picasso.get()
-                .load(EXTRA_URL)
+                .load("https://cf.bstatic.com/images/hotel/max1024x768/226/226214922.jpg")
                 .into(circleImage);
+    }
+
+    private void jsonParse() {
+        String url = "https://moodup.team/test/info.php";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("ingredients");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                String ingredient = jsonArray.getString(i);
+                                mTextViewResult.append(ingredient+"\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 
     public void showOptions(View view) {
         Intent intent = new Intent(this, OptionsActivity.class);
-        //intent.putExtra(OPTION_URL, EXTRA_URL);
         startActivity(intent);
-        //view.findViewById(R.id.optionsButton).setVisibility(View.INVISIBLE);
     }
 
 }
 
 //Icons made by <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+//Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
