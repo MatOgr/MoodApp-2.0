27a7a22791eca64af33ca9c34a2e2e99a1bafdaf
MatOgr <matjonne@gmail.com>
2020-11-20 20:44:33 +0100

    Recipe page completed

--- app/src/main/java/com/example/moodapp/RecipeActivity.java
+++ app/src/main/java/com/example/moodapp/RecipeActivity.java
@@ -13,12 +13,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.mikhaellopez.circularimageview.CircularImageView;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import static com.example.moodapp.MainActivity.EXTRA_URL;
 
@@ -33,17 +33,43 @@ public class RecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe);
 
         mQueue = Volley.newRequestQueue(this);
-        jsonParseImgs(EXTRA_URL);
+        jsonParse(EXTRA_URL);
 
     }
 
-    private void jsonParseImgs(String url) {
+    private void jsonParse(String url) {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            JSONArray jsonArray = response.getJSONArray("imgs");
+                            TextView tmpTV = findViewById(R.id.dishTitle);
+                                    //  dish name
+                            tmpTV.setText(response.getString("title"));
+                            //  dish description
+                            tmpTV = findViewById(R.id.dishDescription);
+                            tmpTV.setText(response.getString("description"));
+
+
+                            //  ingredients
+                            JSONArray jsonArray = response.getJSONArray("ingredients");
+                            tmpTV = findViewById(R.id.ingredientsDescription);
+                            tmpTV.setText("");
+                            for(int i = 0; i < jsonArray.length() - 1; i++) {
+                                tmpTV.append("- " + jsonArray.getString(i) + "\n");
+                            }
+                            tmpTV.append("- " + jsonArray.getString(jsonArray.length()-1));
+
+                            //  preparing
+                            tmpTV = findViewById(R.id.preparingDescription);
+                            tmpTV.setText("");
+                            jsonArray = response.getJSONArray("preparing");
+                            for(int i = 0; i < jsonArray.length() - 1; i++) {
+                                tmpTV.append(String.format("%d. %s\n\n", i + 1, jsonArray.getString(i)));
+                            } tmpTV.append(String.format("%d. %s", jsonArray.length(), jsonArray.getString(jsonArray.length()-1)));
+
+                            // images
+                            jsonArray = response.getJSONArray("imgs");
 
                             Picasso.get()
                                     .load(jsonArray.getString(1)
@@ -66,6 +92,11 @@ public class RecipeActivity extends AppCompatActivity {
         });
 
         mQueue.add(request);
+    }
+
+    // TODO: 20.11.2020 downloading image
+    public void downloadImage(){
+
     }
 
     public void goMain(View view) {
